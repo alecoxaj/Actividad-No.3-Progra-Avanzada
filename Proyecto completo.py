ec4b92c0ba2a1988166a124544d3d1896e7c8811
Marvin7715 <vasquezmarvin588@gmail.com>
2025-07-17 09:20:39 -0600

    Se coloco el menu de opciones sobre que operacion debo elegir

--- Proyecto completo.py
+++ Proyecto completo.py
@@ -3,8 +3,10 @@ a = float(input("Ingrese el primer número: "))
 
 b = float(input("Ingrese el segundo número: "))
 
-print("La Suma da como resultado: ", a + b)
-print("La Resta da como resultado: ", a - b)
-print("La Multiplicación da como resultado: ", a * b)
-print("La División da como resultado: ", a / b)
+print("\nSeleccione la operación:")
+print("1. Suma")
+print("2. Resta")
+print("3. Multiplicación")
+print("4. División")
 
+opcion = input("Ingrese una opción (1/2/3/4): ")
