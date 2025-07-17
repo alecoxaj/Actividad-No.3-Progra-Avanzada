eb8ab5c6d4fea0e9f1cd688322504ee5e546e375
Alejandro <alejandrocoxaj94@gmail.com>
2025-07-17 09:18:25 -0600

    Alejandro colocó if elif y else, dependiendo de la opción que escoja el usuario.

--- Alejandro Coxaj.py
+++ Alejandro Coxaj.py
@@ -1,4 +1,13 @@
-print("La Suma da como resultado: ", a + b)
-print("La Resta da como resultado: ", a - b)
-print("La Multiplicación da como resultado: ", a * b)
-print("La División da como resultado: ", a / b )
+if opcion == "1":
+    print("Resultado de la suma:", a + b)
+elif opcion == "2":
+    print("Resultado de la resta:", a - b)
+elif opcion == "3":
+    print("Resultado de la multiplicación:", a * b)
+elif opcion == "4":
+    if b != 0:
+        print("Resultado de la división:", a / b)
+    else:
+        print("Error: No se puede dividir entre cero.")
+else:
+    print("Opción no válida.")
