9b06cadca25b16b6f8d3c444e9bf42e99ad9056a
Alejandro <alejandrocoxaj94@gmail.com>
2025-07-16 18:26:23 -0600

    Alejandro Coxaj colocó los prints indicando el tipo de operación, y llamando a las variables definidas para que haga las operaciones.

--- Alejandro Coxaj.py
+++ Alejandro Coxaj.py
@@ -1,4 +1,4 @@
 print("La Suma da como resultado: ", a + b)
 print("La Resta da como resultado: ", a - b)
 print("La Multiplicación da como resultado: ", a * b)
-print("La División da como resultado: ", a / b)
+print("La División da como resultado: ", a / b )
