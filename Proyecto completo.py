b6e5bb1e6c7b8dd9bcd76407e4189d41401aaf13
Alejandro <alejandrocoxaj94@gmail.com>
2025-07-16 18:29:02 -0600

    Alejandro juntó el proyecto y Marvin revisó para verificar si no hay errores, y funciona correctamente!

---
+++ Proyecto completo.py
@@ -0,0 +1,9 @@
+a = float(input("Ingrese el primer número: "))
+
+b = float(input("Ingrese el segundo número: "))
+
+print("La Suma da como resultado: ", a + b)
+print("La Resta da como resultado: ", a - b)
+print("La Multiplicación da como resultado: ", a * b)
+print("La División da como resultado: ", a / b )
+
